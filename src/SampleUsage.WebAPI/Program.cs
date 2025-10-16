e1d55b79dbdf2a8173cc80187489179fa224f87e
Nauman Mohammed <nauman.rehman@gmail.com>
2025-10-16 19:28:51 +0530

    Refactor `Program.cs` to simplify code and remove unused HTTPS and redundant sample data configuration.

--- src/SampleUsage.WebAPI/Program.cs
+++ src/SampleUsage.WebAPI/Program.cs
@@ -5,7 +5,6 @@ using SampleUsage.WebAPI.Application.Weather.Queries;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddMediator(Assembly.GetExecutingAssembly());
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -19,24 +18,19 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/weatherforecast", async (IMediator mediator) =>
     {
         var query = new GetWeatherForecastsQuery();
         var forecasts = await mediator.SendAsync(query);
 
-        return forecasts
-            .Select(o => new WeatherForecast(o.Date, o.TemperatureC, o.Summary, o.TemperatureF))
+        var translatedForecasts = forecasts
+            .Select(o => new WeatherForecast(o.Date, o.TemperatureC, o.TemperatureF, o.Summary))
             .ToArray();
+
+        return translatedForecasts;
     })
     .WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary, int TemperatureF);
+record WeatherForecast(DateOnly Date, int TemperatureC, int TemperatureF, string? Summary);
